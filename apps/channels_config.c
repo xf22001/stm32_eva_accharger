2ff20df3a815a91b4badcf96c283ad9eca8e83a7
xiaofei <alibaba22001@163.com>
2021-06-06 20:09:11 +0800

    update

--- apps/channels_config.c
+++ apps/channels_config.c
@@ -1,12 +1,12 @@
 
 
 /*================================================================
- *   
- *   
+ *
+ *
  *   文件名称：channels_config.c
  *   创 建 者：肖飞
  *   创建日期：2021年01月18日 星期一 09时26分44秒
- *   修改日期：2021年06月05日 星期六 13时51分37秒
+ *   修改日期：2021年06月06日 星期日 19时42分11秒
  *   描    述：
  *
  *================================================================*/
@@ -117,10 +117,8 @@ static channel_config_t *channel_config_sz[] = {
 
 static channels_config_t channels_config_0 = {
 	.id = 0,
-	.channels_config = {
-		.channels_number = ARRAY_SIZE(channel_config_sz),
-		.channel_config = channel_config_sz,
-	},
+	.channel_number = ARRAY_SIZE(channel_config_sz),
+	.channel_config = channel_config_sz,
 	.power_module_config = {
 		.channels_power_module_number = 0,
 		.hcan_power = NULL,
