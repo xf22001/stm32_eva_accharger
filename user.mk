6f6fff0d1ba52753869be8d621c1b98fbae5c439
xiaofei <alibaba22001@163.com>
2021-06-19 13:39:53 +0800

    update

--- user.mk
+++ user.mk
@@ -6,7 +6,7 @@
 #   文件名称：user.mk
 #   创 建 者：肖飞
 #   创建日期：2019年10月25日 星期五 13时04分38秒
-#   修改日期：2021年06月16日 星期三 17时23分40秒
+#   修改日期：2021年06月19日 星期六 12时30分36秒
 #   描    述：
 #
 #================================================================
@@ -91,6 +91,7 @@ USER_C_SOURCES += apps/modules/app/charger/channel.c
 USER_C_SOURCES += apps/modules/app/charger/channel_handler_dc.c
 USER_C_SOURCES += apps/modules/app/charger/channel_handler_ac.c
 USER_C_SOURCES += apps/modules/app/charger/channel_handler_proxy.c
+USER_C_SOURCES += apps/modules/app/charger/channel_record_handler.c
 USER_C_SOURCES += apps/modules/app/charger/charger.c
 USER_C_SOURCES += apps/modules/app/charger/charger_bms.c
 USER_C_SOURCES += apps/modules/app/charger/charger_bms_gb.c
