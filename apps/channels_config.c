aaef53f02f0b64688394a492b46661dc2428fadd
xiaofei <alibaba22001@163.com>
2021-09-12 12:31:34 +0800

    update

--- apps/channels_config.c
+++ apps/channels_config.c
@@ -6,7 +6,7 @@
  *   文件名称：channels_config.c
  *   创 建 者：肖飞
  *   创建日期：2021年01月18日 星期一 09时26分44秒
- *   修改日期：2021年09月12日 星期日 10时03分00秒
+ *   修改日期：2021年09月12日 星期日 12时25分26秒
  *   描    述：
  *
  *================================================================*/
@@ -206,6 +206,7 @@ static channels_config_t channels_config_0 = {
 		.clk_pin = voi_clk_Pin,
 	},
 	.card_reader_config = {
+		.default_type = CARD_READER_TYPE_MT_318_626,
 		.size = ARRAY_SIZE(card_reader_config_item_sz),
 		.items = card_reader_config_item_sz,
 	},
